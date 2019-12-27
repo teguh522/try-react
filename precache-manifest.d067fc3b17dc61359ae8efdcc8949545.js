4eed1e6cf9cc8e178b96078f3edac7f5d719f12c
Teguh Firmansyah <teguhf522@gmail.com>
2019-12-27 14:04:42 +0700

    Updates

---
+++ precache-manifest.d067fc3b17dc61359ae8efdcc8949545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f3a0961f644b2d13141c3f230b8da4",
+    "url": "/try-react/index.html"
+  },
+  {
+    "revision": "f3967852b5114c758648",
+    "url": "/try-react/static/css/main.5f466e5c.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/try-react/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/try-react/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3967852b5114c758648",
+    "url": "/try-react/static/js/main.7de23a61.chunk.js"
+  },
+  {
+    "revision": "413a92b6d0556bfcaf30",
+    "url": "/try-react/static/js/runtime-main.f4c42113.js"
+  }
+]);
